90e1417388acfb5d88b1d28d5d19f790a3441dd5
andrescarlos2211 <andrescarlos2211@gmail.com>
2023-03-11 13:55:34 -0300

    typo

--- main.js
+++ main.js
@@ -12,12 +12,11 @@ app.get('/', (req, res) =>{(
 )})
 
 app.get('/resultados/:piloto/:minutos/:uCierre/:completa', (req,res)=> {
-let piloto = req.params.piloto
-let minutos = req.params.minutos
-let uCierre = req.params.uCierre
-let completa = req.params.completa
-let p1 = JSON.stringify(piloto) 
-res.send(p1)
+let piloto = JSON.stringify(req.params.piloto)
+let minutos = JSON.stringify(req.params.minutos)
+let uCierre = JSON.stringify(req.params.uCierre)
+let completa = JSON.stringify(req.params.completa)
+let p1 = piloto
 })
 
 
